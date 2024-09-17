03d2c333f8257b53ab7036d736937f8fe8f10602
johnnybirds05 <johnmichaelcagadas@gmail.com>
2024-09-17 14:02:52 +0800

    updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import React from "react";
-import ReactDom from "react-dom/client";
+import React from 'react'
+import ReactDom from 'react-dom/client'
 
-function Greeting() {
-  return <h3>Hello</h3>;
+const BookList = () => {
+  return (
+    <>
+      <section>
+        <Book></Book>
+        <Book></Book>
+        <Book></Book>
+        <Book></Book>
+      </section>
+    </>
+  )
 }
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  )
+}
 
-root.render(<Greeting></Greeting>);
+const Image = () => (
+  <img src="https://m.media-amazon.com/images/I/913C+MR3S5L._AC_UL320_.jpg"></img>
+)
+const Title = () => <h3>The Women: A Novel</h3>
+const Author = () => <span>by: Kristin Hannah</span>
+
+const root = ReactDom.createRoot(document.getElementById('root'))
+
+root.render(<BookList></BookList>)
