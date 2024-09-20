171d9a47154feeb4303fb08df6c7f08fb5a04992
AreYouLucky <johnmichaelcagadas@gmail.com>
2024-09-20 14:34:50 +0800

    updates

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
+import './index.css'
+
+const author = 'Kristin Hannah'
+const title = 'The Women: A Novel'
 
 const BookList = () => {
   return (
-    <>
-      <section>
-        <Book></Book>
-        <Book></Book>
-        <Book></Book>
-        <Book></Book>
-      </section>
-    </>
+    <section className="booklist">
+      <Book author={author} title={title}></Book>
+      <Book author={author} title={title}></Book>
+    </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
   return (
-    <article>
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
+    <article className="book">
+      <img src="https://m.media-amazon.com/images/I/913C+MR3S5L._AC_UL320_.jpg"></img>
+      <h3>{props.title}</h3>
+      <span style={authorStyle}>by: {props.author}</span>
     </article>
   )
 }
 
-const Image = () => (
-  <img src="https://m.media-amazon.com/images/I/913C+MR3S5L._AC_UL320_.jpg"></img>
-)
-const Title = () => <h3>The Women: A Novel</h3>
-const Author = () => <span>by: Kristin Hannah</span>
-
 const root = ReactDom.createRoot(document.getElementById('root'))
 
 root.render(<BookList></BookList>)
+
+const authorStyle = {
+  marginTop: '1.2rem',
+  color: 'GrayText',
+  fontSize: '0.85rem',
+}
